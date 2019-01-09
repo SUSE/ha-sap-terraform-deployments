84e8299417a7567539462c8cb30d64891e5da6ab
Xabier Arbulu Insausti <xarbulu@suse.com>
2019-01-09 08:20:04 +0000

    Add variables file to store default variables

---
+++ libvirt/terraform/variables.tf
@@ -0,0 +1,38 @@
+variable "qemu_uri" {
+	description = "URI to connect with the qemu-service."
+	default = "qemu:///system"
+}
+
+variable "base_image" {
+  description = "Image of the sap hana nodes"
+  default = "http://download.suse.de/ibs/Devel:/Galaxy:/Terraform:/Images/images/sles15.x86_64.qcow2"
+}
+
+variable "iprange" {
+  description = "IP range of the isolated network"
+  default = "192.168.106.0/24"
+}
+
+variable "name_prefix" {
+  description = "Prefix of the deployment VM, network and disks"
+  default = "hanatest"
+}
+
+variable "sap_inst_media" {
+  description = "url to the media where SAP software is stored. This media shall be mounted in /root/sap_inst"
+  type = "string"
+}
+
+variable "host_ips" {
+  description = "IP addresses of the nodes"
+  default = ["192.168.106.15", "192.168.106.16"]
+}
+
+variable "additional_repos" {
+  description = "Map of the repositories to add to the images. Repo name = url"
+  default = {
+    "SLE-15-x86_64-Update" = "http://download.suse.de/ibs/SUSE/Updates/SLE-Module-Basesystem/15/x86_64/update/"
+    "SLE-15-x86_64-Pool" = "http://download.suse.de/ibs/SUSE/Products/SLE-Module-Basesystem/15/x86_64/product/"
+    "SLE-15-x86_64-Source" = "http://download.suse.de/ibs/SUSE/Products/SLE-Module-Basesystem/15/x86_64/product_source/"
+  }
+}
