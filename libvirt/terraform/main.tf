be2424f1de9531a7598705df68a6bd63da6396b4
Loic Devulder <ldevulder@suse.de>
2019-02-08 11:38:42 +0100

    Revert "Add libvirt to travis"

--- libvirt/terraform/main.tf
+++ libvirt/terraform/main.tf
@@ -3,46 +3,47 @@ provider "libvirt" {
 }
 
 module "base" {
-  source  = "./modules/base"
-  image   = "${var.base_image}"
+  source = "./modules/base"
+  image = "${var.base_image}"
   iprange = "${var.iprange}"
 
   // optional parameters below
   name_prefix = "${var.name_prefix}"
-
   // pool = "default"
   pool = "terraform"
-
   // network_name = "default"
   network_name = ""
-  bridge       = "br0"
-  timezone     = "Europe/Berlin"
+  bridge = "br0"
+  timezone = "Europe/Berlin"
 }
 
 module "sbd_disk" {
-  source             = "./modules/sbd"
+  source = "./modules/sbd"
   base_configuration = "${module.base.configuration}"
-  sbd_disk_size      = "104857600"
+  sbd_disk_size = "104857600"
 }
 
 module "hana_node" {
-  source             = "./modules/hana_node"
+  source = "./modules/hana_node"
   base_configuration = "${module.base.configuration}"
-
   // hana01 and hana02
 
-  name  = "hana"
+  name = "hana"
   count = 2
-  vcpu   = 4
+
+  vcpu = 4
   memory = 32678
+
   sap_inst_media = "${var.sap_inst_media}"
-  ntp_server     = "pool.ntp.org"
+  ntp_server = "pool.ntp.org"
   hana_disk_size = "68719476736"
-  host_ips       = "${var.host_ips}"
-  sbd_disk_id    = "${module.sbd_disk.id}"
+  host_ips = "${var.host_ips}"
+  sbd_disk_id = "${module.sbd_disk.id}"
+
   # Set proper ssh file. The default files are public in github
   # Copy custom files in salt/hana_node/file/sshkeys
   cluster_ssh_pub = "${var.cluster_ssh_pub}"
   cluster_ssh_key = "${var.cluster_ssh_key}"
+
   additional_repos = "${var.additional_repos}"
 }
--- libvirt/terraform/modules/host/variables.tf
+++ libvirt/terraform/modules/host/variables.tf
@@ -1,73 +1,72 @@
 variable "base_configuration" {
   description = "use ${module.base.configuration}, see the main.tf example file"
-  type        = "map"
+  type = "map"
 }
 
 variable "name" {
   description = "hostname, without the domain part"
-  type        = "string"
+  type = "string"
 }
 
 variable "additional_repos" {
   description = "extra repositories in the form {label = url}"
-  default     = {}
+  default = {}
 }
 
 variable "additional_packages" {
   description = "extra packages to install"
-  default     = []
+  default = []
 }
 
-variable "count" {
+variable "count"  {
   description = "number of hosts like this one"
-  default     = 1
+  default = 1
 }
 
 variable "grains" {
   description = "custom grain string to be added to this host's configuration"
-  default     = ""
+  default = ""
 }
 
 variable "ssh_key_path" {
   description = "path of additional pub ssh key you want to use to access VMs"
-  default     = "/dev/null"
-
+  default = "/dev/null"
   # HACK: "" cannot be used as a default because of https://github.com/hashicorp/hil/issues/50
 }
 
 variable "hana_disk_size" {
   description = "hana partition disk size"
-  default     = "68719476736"              # 64GB
+  default = "68719476736" # 64GB
 }
 
 variable "host_ips" {
   description = "ip addresses to set to the nodes"
-  type        = "list"
+  type = "list"
 }
 
 // Provider-specific variables
 
 variable "memory" {
   description = "RAM memory in MiB"
-  default     = 512
+  default = 512
 }
 
 variable "vcpu" {
   description = "number of virtual CPUs"
-  default     = 1
+  default = 1
 }
 
 variable "running" {
   description = "whether this host should be turned on or off"
-  default     = true
+  default = true
 }
 
 variable "mac" {
   description = "a MAC address in the form AA:BB:CC:11:22:22"
-  default     = ""
+  default = ""
 }
 
 variable "additional_disk" {
   description = "disk block definition(s) to be added to this host"
-  default     = []
+  default = []
 }
