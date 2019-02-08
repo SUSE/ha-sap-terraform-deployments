fd87bc04976f7cad108ea79690ebb1a073182a3c
Diego Akechi <dakechi@suse.com>
2019-02-08 16:09:44 +0100

    Include Registration Option for SUSE distributions (#28)
    * Include Registration Option for SUSE distributions
    Include support to register the system to the SUSE Customer Center
    Include also support to register additional modules and extensions
    
    * Use the Jinja ~ string operator instead of the str()
    Improves readability and avoids errors of function not found.
    
    * Format terraform files according fmt command

--- libvirt/terraform/modules/hana_node/main.tf
+++ libvirt/terraform/modules/hana_node/main.tf
@@ -1,13 +1,16 @@
 module "hana_node" {
   source = "../host"
 
-  base_configuration  = "${var.base_configuration}"
-  name                = "${var.name}"
-  count               = "${var.count}"
-  additional_repos    = "${var.additional_repos}"
-  additional_packages = "${var.additional_packages}"
-  ssh_key_path        = "${var.ssh_key_path}"
-  host_ips            = "${var.host_ips}"
+  base_configuration     = "${var.base_configuration}"
+  name                   = "${var.name}"
+  count                  = "${var.count}"
+  reg_code               = "${var.reg_code}"
+  reg_email              = "${var.reg_email}"
+  reg_additional_modules = "${var.reg_additional_modules}"
+  additional_repos       = "${var.additional_repos}"
+  additional_packages    = "${var.additional_packages}"
+  ssh_key_path           = "${var.ssh_key_path}"
+  host_ips               = "${var.host_ips}"
 
   grains = <<EOF
 
