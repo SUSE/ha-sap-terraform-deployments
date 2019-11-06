7165ad5da75025094f34bb5b05d746c5e1ad5394
dmaiocchi <dmaiocchi@suse.com>
2019-11-06 10:39:25 +0100

    experimental

--- salt/monitoring/container/run-grafana.sh
+++ salt/monitoring/container/run-grafana.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
-
-docker run -p 3000:3000 --name grafana-host -v "$PWD"/../provisioning/:/etc/grafana/provisioning -e "GF_provisioning=/provisioning" grafana/grafana
+docker stop grafana || true && docker rm grafana || true
+docker run -p 3000:3000 --name grafana -v "$PWD"/../provisioning/:/etc/grafana/provisioning -e "GF_provisioning=/provisioning" grafana/grafa
