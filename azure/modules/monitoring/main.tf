4d7a9477f4ee39112888cda7344e770e65f69cc3
Simranpal <simranpal.singh@suse.com>
2020-01-24 17:00:01 -0800

    Create Monitoring terraform module

---
+++ azure/modules/monitoring/main.tf
@@ -0,0 +1,126 @@
+# Availability set for the monitoring VMs
+
+resource "azurerm_availability_set" "hana-availability-set" {
+  name                        = "avset-monitoring"
+  location                    = var.az_region
+  resource_group_name         = var.resource_group_name
+  managed                     = "true"
+  platform_fault_domain_count = 2
+
+  tags = {
+    workspace = terraform.workspace
+  }
+}
+
+# monitoring network configuration
+
+resource "azurerm_network_interface" "monitoring" {
+  name                      = "nic-monitoring"
+  count                     = var.monitoring_enabled == true ? 1 : 0
+  location                  = var.az_region
+  resource_group_name       = var.resource_group_name
+  network_security_group_id = var.sec_group_id
+
+  ip_configuration {
+    name                          = "ipconf-primary"
+    subnet_id                     = var.network_subnet_id
+    private_ip_address_allocation = "static"
+    private_ip_address            = var.monitoring_srv_ip
+    public_ip_address_id          = azurerm_public_ip.monitoring.0.id
+  }
+
+  tags = {
+    workspace = terraform.workspace
+  }
+}
+
+resource "azurerm_public_ip" "monitoring" {
+  name                    = "pip-monitoring"
+  count                   = var.monitoring_enabled == true ? 1 : 0
+  location                = var.az_region
+  resource_group_name     = var.resource_group_name
+  allocation_method       = "Dynamic"
+  idle_timeout_in_minutes = 30
+
+  tags = {
+    workspace = terraform.workspace
+  }
+}
+
+# monitoring custom image. only available if monitoring_image_uri is used
+
+resource "azurerm_image" "monitoring" {
+  count               = var.monitoring_uri != "" ? 1 : 0
+  name                = "monitoringSrvImg"
+  location            = var.az_region
+  resource_group_name = var.resource_group_name
+
+  os_disk {
+    os_type  = "Linux"
+    os_state = "Generalized"
+    blob_uri = var.monitoring_uri
+    size_gb  = "32"
+  }
+
+  tags = {
+    workspace = terraform.workspace
+  }
+}
+
+# monitoring VM
+
+resource "azurerm_virtual_machine" "monitoring" {
+  name                  = "vmmonitoring"
+  count                 = var.monitoring_enabled == true ? 1 : 0
+  location              = var.az_region
+  resource_group_name   = var.resource_group_name
+  network_interface_ids = [azurerm_network_interface.monitoring.0.id]
+  vm_size               = var.instancetype
+
+  storage_os_disk {
+    name              = "disk-monitoring-Os"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Premium_LRS"
+  }
+
+  storage_image_reference {
+    id        = var.monitoring_uri != "" ? azurerm_image.monitoring.0.id : ""
+    publisher = var.monitoring_uri != "" ? "" : var.monitoring_public_publisher
+    offer     = var.monitoring_uri != "" ? "" : var.monitoring_public_offer
+    sku       = var.monitoring_uri != "" ? "" : var.monitoring_public_sku
+    version   = var.monitoring_uri != "" ? "" : var.monitoring_public_version
+  }
+
+  storage_data_disk {
+    name              = "disk-monitoring-Data01"
+    caching           = "ReadWrite"
+    create_option     = "Empty"
+    disk_size_gb      = "10"
+    lun               = "0"
+    managed_disk_type = "Standard_LRS"
+  }
+
+  os_profile {
+    computer_name  = "monitoring"
+    admin_username = var.admin_user
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = true
+
+    ssh_keys {
+      path     = "/home/${var.admin_user}/.ssh/authorized_keys"
+      key_data = file(var.public_key_location)
+    }
+  }
+
+  boot_diagnostics {
+    enabled     = "true"
+    storage_uri = var.storage_account
+  }
+
+  tags = {
+    workspace = terraform.workspace
+  }
+}
--- azure/outputs.tf
+++ azure/outputs.tf
@@ -70,32 +70,20 @@ output "cluster_nodes_public_name" {
 
 # Monitoring
 
-data "azurerm_public_ip" "monitoring" {
-  count               = var.monitoring_enabled == true ? 1 : 0
-  name                = azurerm_public_ip.monitoring.0.name
-  resource_group_name = azurerm_virtual_machine.monitoring.0.resource_group_name
-}
-
-data "azurerm_network_interface" "monitoring" {
-  count               = var.monitoring_enabled == true ? 1 : 0
-  name                = azurerm_network_interface.monitoring.0.name
-  resource_group_name = azurerm_virtual_machine.monitoring.0.resource_group_name
-}
-
 output "monitoring_ip" {
-  value = join("", data.azurerm_network_interface.monitoring.*.private_ip_address)
+  value = module.monitoring.monitoring_ip
 }
 
 output "monitoring_public_ip" {
-  value = join("", data.azurerm_public_ip.monitoring.*.ip_address)
+  value = module.monitoring.monitoring_public_ip
 }
 
 output "monitoring_name" {
-  value = join("", azurerm_virtual_machine.monitoring.*.name)
+  value = module.monitoring.monitoring_name
 }
 
 output "monitoring_public_name" {
-  value = join("", data.azurerm_public_ip.monitoring.*.fqdn)
+  value = module.monitoring.monitoring_public_name
 }
 
 # drbd
