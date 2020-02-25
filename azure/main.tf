cbb444f453921c30e044490243807dd2873310f6
Simranpal <simranpal.singh@suse.com>
2020-02-25 15:37:40 -0800

    Format terraform spacing  in main.tf

--- azure/main.tf
+++ azure/main.tf
@@ -145,7 +145,7 @@ module "monitoring" {
   drbd_enabled                = var.drbd_enabled
   drbd_ips                    = var.drbd_ips
   netweaver_enabled           = var.netweaver_enabled
-  netweaver_ips                = var.netweaver_ips
+  netweaver_ips               = var.netweaver_ips
 }
 
 module "iscsi_server" {
