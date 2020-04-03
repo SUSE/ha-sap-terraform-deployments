d8dbecfbc18b5d757953eacd4781b4a010f8dc70
simran <simranpal.singh@suse.com>
2020-04-02 21:41:39 -0700

    Add sapcar related pillar entries and variables for Libvirt

--- libvirt/modules/netweaver_node/variables.tf
+++ libvirt/modules/netweaver_node/variables.tf
@@ -77,6 +77,24 @@ variable "netweaver_swpm_folder" {
   default     = ""
 }
 
+variable "nw_sapcar_exe" {
+  description = "Path to sapcar executable, relative from the `netweaver_inst_media` mounted point"
+  type        = string
+  default     = ""
+}
+
+variable "nw_swpm_sar" {
+  description = "SWPM installer sar archive containing the installer, path relative from the `netweaver_inst_media` mounted point"
+  type        = string
+  default     = ""
+}
+
+variable "nw_swpm_extract_dir" {
+  description = "Extraction path for Netweaver software SWPM folder, if SWPM sar file is provided"
+  type        = string
+  default     = ""
+}
+
 variable "netweaver_sapexe_folder" {
   description = "Software folder where needed sapexe `SAR` executables are stored (sapexe, sapexedb, saphostagent), path relative from the `netweaver_inst_media` mounted point"
   type        = string
