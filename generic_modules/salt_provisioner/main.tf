c6f6323d0e5fa6b10ab9c48377b3a058e3e0e1f0
Xabier Arbulu Insausti <xarbulu@suse.com>
2020-04-08 10:21:35 +0100

    Create the salt_provisioner generic tf module

---
+++ generic_modules/salt_provisioner/main.tf
@@ -0,0 +1,65 @@
+resource "null_resource" "provision_background" {
+  count = var.background ? var.node_count : 0
+  triggers = {
+    triggers = join(",", var.instance_ids)
+  }
+
+  connection {
+    host        = element(var.public_ips, count.index)
+    type        = "ssh"
+    user        = var.user
+    password    = var.password
+    private_key = var.private_key_location != "" ? file(var.private_key_location) : ""
+  }
+
+  provisioner "file" {
+    source      = "../salt"
+    destination = "/tmp"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "nohup sudo sh /tmp/salt/provision.sh > /tmp/provisioning.log &",
+      "return_code=$? && sleep 1 && exit $return_code",
+    ] # Workaround to let the process start in background properly
+  }
+}
+
+resource "null_resource" "provision" {
+  count = ! var.background ? var.node_count : 0
+  triggers = {
+    triggers = join(",", var.instance_ids)
+  }
+
+  connection {
+    host        = element(var.public_ips, count.index)
+    type        = "ssh"
+    user        = var.user
+    password    = var.password
+    private_key = var.private_key_location != "" ? file(var.private_key_location) : ""
+  }
+
+  provisioner "file" {
+    source      = "../salt"
+    destination = "/tmp"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo sh /tmp/salt/provision.sh -so > /tmp/provisioning.log",
+    ]
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "[ -f /var/run/reboot-needed ] && echo \"Rebooting the machine...\" && (nohup sudo sh -c 'systemctl stop sshd;/sbin/reboot' &) && sleep 5",
+    ]
+    on_failure = continue
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo sh /root/salt/provision.sh -pdq >> /tmp/provisioning.log",
+    ]
+  }
+}
---
+++ generic_modules/salt_provisioner/variables.tf
@@ -0,0 +1,38 @@
+variable "node_count" {
+  description = "Number of nodes to run the provisioner"
+  type        = number
+}
+
+variable "instance_ids" {
+  description = "List with the instance ids that will trigger the provisioner"
+  type        = list(string)
+}
+
+variable "user" {
+  description = "User for the SSH connection"
+  type        = string
+  default     = "root"
+}
+
+variable "password" {
+  description = "Password for the SSH connection"
+  type        = string
+  default     = ""
+}
+
+variable "private_key_location" {
+  description = "SSH private key for the connection. It has priority over password variable"
+  type        = string
+  default     = ""
+}
+
+variable "public_ips" {
+  description = "List of ips used to connect through SSH"
+  type        = list(string)
+}
+
+variable "background" {
+  description = "Execute the provisioning in background"
+  type        = bool
+  default     = false
+}
