b91ae3ed10cd1a29c3cccb20ec29b315750fd5ec
simran <simranpal.singh@suse.com>
2020-04-07 18:51:37 -0700

    Fix the output types for iscsisrv variables

--- aws/modules/iscsi_server/outputs.tf
+++ aws/modules/iscsi_server/outputs.tf
@@ -3,17 +3,17 @@ data "aws_instance" "iscsisrv" {
 }
 
 output "iscsisrv_ip" {
-  value = [data.aws_instance.iscsisrv.private_ip]
+  value = data.aws_instance.iscsisrv.private_ip
 }
 
 output "iscsisrv_public_ip" {
-  value = [data.aws_instance.iscsisrv.public_ip]
+  value = data.aws_instance.iscsisrv.public_ip
 }
 
 output "iscsisrv_name" {
-  value = [data.aws_instance.iscsisrv.id]
+  value = data.aws_instance.iscsisrv.id
 }
 
 output "iscsisrv_public_name" {
-  value = [data.aws_instance.iscsisrv.public_dns]
+  value = data.aws_instance.iscsisrv.public_dns
 }
