3d82f59980d2aa7af6e3780f1f2e7e90694642e0
Xabier Arbulu Insausti <xarbulu@suse.com>
2020-04-28 13:35:58 +0100

    Add hana_cluster_vip to grains

--- libvirt/modules/hana_node/salt_provisioner.tf
+++ libvirt/modules/hana_node/salt_provisioner.tf
@@ -37,6 +37,7 @@ sbd_disk_device: "${var.shared_storage_type == "iscsi" ? "/dev/sda" : "/dev/vdc"
 iscsi_srv_ip: ${var.iscsi_srv_ip}
 qa_mode: ${var.qa_mode}
 hwcct: ${var.hwcct}
+hana_cluster_vip: ${var.hana_cluster_vip}
 hana_fstype: ${var.hana_fstype}
 hana_inst_folder: ${var.hana_inst_folder}
 hana_platform_folder: ${var.hana_platform_folder}
