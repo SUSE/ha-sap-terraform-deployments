33551cd8d5c4f2cf206b2f43743d76d2a6031a8e
simran <simranpal.singh@suse.com>
2020-05-06 19:02:01 -0700

    Update hana_ip for AWS netweaver nodes to use automatic generated IP

--- aws/main.tf
+++ aws/main.tf
@@ -132,7 +132,7 @@ module "netweaver_node" {
   netweaver_sapexe_folder    = var.netweaver_sapexe_folder
   netweaver_additional_dvds  = var.netweaver_additional_dvds
   netweaver_nfs_share        = var.drbd_enabled ? "${local.drbd_cluster_vip}:/HA1" : "${aws_efs_file_system.netweaver-efs.0.dns_name}:"
-  hana_ip                    = var.hana_cluster_vip
+  hana_ip                    = local.hana_cluster_vip
   host_ips                   = local.netweaver_ips
   virtual_host_ips           = local.netweaver_virtual_ips
   public_key_location        = var.public_key_location
