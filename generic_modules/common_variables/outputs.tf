1ebe7e963b86412aba1e9f6dd7042d1a727e4291
Xabier Arbulu Insausti <xarbulu@suse.com>
2020-09-10 13:30:58 +0200

    Add provisioning_log_level variable

--- generic_modules/common_variables/outputs.tf
+++ generic_modules/common_variables/outputs.tf
@@ -9,6 +9,7 @@ output "configuration" {
     public_key_location    = var.public_key_location
     private_key_location   = var.private_key_location
     provisioner            = var.provisioner
+    provisioning_log_level = var.provisioning_log_level
     background             = var.background
     monitoring_enabled     = var.monitoring_enabled
     monitoring_srv_ip      = var.monitoring_srv_ip
@@ -23,6 +24,7 @@ additional_packages: [${join(", ", formatlist("'%s'", var.additional_packages))}
 monitoring_enabled: ${var.monitoring_enabled}
 monitoring_srv_ip: ${var.monitoring_srv_ip}
 qa_mode: ${var.qa_mode}
+provisioning_log_level: ${var.provisioning_log_level}
 EOF
   }
 }
