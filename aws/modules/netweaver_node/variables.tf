aa3cfea0e285993bf7f2201beadbc88c121835cc
Xabier Arbulu Insausti <xarbulu@suse.com>
2020-10-13 15:03:07 +0200

    Add add new hana/nw variables for s4hana in aws

--- aws/modules/netweaver_node/variables.tf
+++ aws/modules/netweaver_node/variables.tf
@@ -2,9 +2,14 @@ variable "common_variables" {
   description = "Output of the common_variables module"
 }
 
-variable "netweaver_count" {
-  type    = string
-  default = "4"
+variable "xscs_server_count" {
+  type    = number
+  default = 2
+}
+
+variable "app_server_count" {
+  type    = number
+  default = 2
 }
 
 variable "instance_type" {
@@ -144,6 +149,21 @@ variable "hana_ip" {
   default     = "192.168.1.10"
 }
 
+variable "hana_sid" {
+  description = "System identifier of the HANA system (e.g.: HA1 or PRD)"
+  type        = string
+}
+
+variable "hana_instance_number" {
+  description = "Instance number of the HANA system. It must be a 2 digits string. Examples: 00, 01, 10"
+  type        = string
+}
+
+variable "hana_master_password" {
+  description = "Master password for the HANA system (sidadm user included)"
+  type        = string
+}
+
 variable "host_ips" {
   description = "ip addresses of the machines.  The addresses must belong to the the subnet provided in subnet_address_range"
   type        = list(string)
@@ -178,12 +198,6 @@ variable "pas_instance_number" {
   description = "PAS instance number"
   type        = string
   default     = "01"
-}
-
-variable "aas_instance_number" {
-  description = "AAS instance number"
-  type        = string
-  default     = "02"
 }
 
 variable "ha_enabled" {
