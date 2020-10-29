e373d663e59035bae9a8c91f03f0029d74c3b58f
Xabier Arbulu Insausti <xarbulu@suse.com>
2020-10-29 08:44:31 +0100

    Add comment on depends_on usage in output files

--- azure/modules/monitoring/outputs.tf
+++ azure/modules/monitoring/outputs.tf
@@ -2,6 +2,7 @@ data "azurerm_public_ip" "monitoring" {
   count               = local.bastion_enabled == false && var.monitoring_enabled == true ? 1 : 0
   name                = azurerm_public_ip.monitoring.0.name
   resource_group_name = azurerm_virtual_machine.monitoring.0.resource_group_name
+  # depends_on is included to avoid the issue with `resource_group was not found`. Find an example in: https://github.com/terraform-providers/terraform-provider-azurerm/issues/8476
   depends_on          = [azurerm_virtual_machine.monitoring]
 }
 
@@ -9,6 +10,7 @@ data "azurerm_network_interface" "monitoring" {
   count               = var.monitoring_enabled == true ? 1 : 0
   name                = azurerm_network_interface.monitoring.0.name
   resource_group_name = azurerm_virtual_machine.monitoring.0.resource_group_name
+  # depends_on is included to avoid the issue with `resource_group was not found`. Find an example in: https://github.com/terraform-providers/terraform-provider-azurerm/issues/8476
   depends_on          = [azurerm_virtual_machine.monitoring]
 }
 
