e29f9a9d3d1521436b598fa026a428c197726ad1
Eike Waldt <waldt@b1-systems.de>
2021-11-10 14:21:08 +0100

    use public bastion IP from fortinet module for fortinet deployments

--- azure/modules/bastion/main.tf
+++ azure/modules/bastion/main.tf
@@ -75,7 +75,7 @@ resource "azurerm_network_interface" "bastion" {
     subnet_id                     = var.snet_id == "" ? azurerm_subnet.bastion[0].id : var.snet_id
     private_ip_address_allocation = "static"
     private_ip_address            = local.private_ip_address
-    public_ip_address_id          = !var.fortinet_enabled ? azurerm_public_ip.bastion[0].id : var.fortinet_bastion_public_ip_id
+    public_ip_address_id          = !var.fortinet_enabled ? azurerm_public_ip.bastion[0].id : ""
   }
 
   tags = {
