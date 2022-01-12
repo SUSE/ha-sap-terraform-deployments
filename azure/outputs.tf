44793d3b308aed2625638e8dfc4a381b90fffe71
John McDonough <movinalot@gmail.com>
2022-01-12 11:06:58 -0500

    add fgt and fadc public and mgmt IP output

--- azure/outputs.tf
+++ azure/outputs.tf
@@ -99,3 +99,23 @@ output "netweaver_public_name" {
 output "bastion_public_ip" {
   value = var.bastion_enabled ? module.bastion.0.public_ip : ""
 }
+
+output "fortigate_virtual_public_ip" {
+  value = var.fortinet_enabled ? format("https://%s",module.fortigate[0].fortigate_virtual_public_ip) : ""
+}
+
+output "fortigate_a_management_public_ip" {
+  value = var.fortinet_enabled ? format("https://%s",module.fortigate[0].fortigate_a_management_public_ip) : ""
+}
+
+output "fortigate_b_management_public_ip" {
+  value = var.fortinet_enabled ? format("https://%s",module.fortigate[0].fortigate_b_management_public_ip) : ""
+}
+
+output "fortiadc_a_management_public_ip" {
+  value = var.fortinet_enabled ? format("https://%s:41443",module.fortigate[0].fortigate_virtual_public_ip) : ""
+}
+
+output "fortiadc_b_management_public_ip" {
+  value = var.fortinet_enabled ? format("https://%s:51443",module.fortigate[0].fortigate_virtual_public_ip) : ""
+}
