ff428d16185bd0beb7d8e29d8ccde08d6180242d
John McDonough <movinalot@gmail.com>
2022-01-12 11:06:58 -0500

    add fgt and fadc public and mgmt IP output

--- azure/modules/fortinet/fortigate/outputs.tf
+++ azure/modules/fortinet/fortigate/outputs.tf
@@ -1,3 +1,14 @@
+output "fortigate_virtual_public_ip" {
+  value = azurerm_public_ip.public_ip["pip-fgt-v"].ip_address
+}
+
+output "fortigate_a_management_public_ip" {
+  value = azurerm_public_ip.public_ip["pip-fgt-a"].ip_address
+}
+
+output "fortigate_b_management_public_ip" {
+  value = azurerm_public_ip.public_ip["pip-fgt-b"].ip_address
+}
 output "bastion_public_ip" {
   value = azurerm_public_ip.public_ip["pip-bastion-lb-fe"].ip_address
 }
