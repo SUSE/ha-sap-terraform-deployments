3115e26aad663a28af0338683c876457031bf4cc
Eike Waldt <waldt@b1-systems.de>
2022-04-28 17:26:26 +0200

    aws: use hana_data_disks_configuration+lvm mechanism from azure

--- aws/modules/hana_node/variables.tf
+++ aws/modules/hana_node/variables.tf
@@ -75,16 +75,6 @@ variable "host_ips" {
   type        = list(string)
 }
 
-variable "hana_data_disk_type" {
-  type    = string
-  default = "gp2"
-}
-
-variable "hana_data_disk_size" {
-  description = "Disk size in GB for the disk used to store HANA database content"
-  type        = number
-}
-
 variable "iscsi_srv_ip" {
   description = "iscsi server address"
   type        = string
@@ -120,3 +110,32 @@ variable "os_owner" {
   description = "OS image owner"
   type        = string
 }
+
+variable "block_devices" {
+  description = "List of devices that will be available to attach as an ebs volume."
+  type        = string
+}
+
+variable "hana_data_disks_configuration" {
+  type        = map(any)
+  default     = {}
+  description = <<EOF
+    This map describes how the disks will be formatted to create the definitive configuration during the provisioning.
+
+    disks_type and disks_size are used during the disks creation. The number of elements must match in all of them
+    "," is used to separate each disk.
+
+    disk_type = The disk type used to create disks. See https://aws.amazon.com/ebs/volume-types/ and https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/ebs_volume for reference.
+    disk_size = The disk size in GB.
+
+    luns, names, lv_sizes and paths are used during the provisioning to create/format/mount logical volumes and filesystems.
+    "#" character is used to split the volume groups, while "," is used to define the logical volumes for each group
+    The number of groups split by "#" must match in all of the entries.
+
+    luns  -> The luns or disks used for each volume group. The number of luns must match with the configured in the previous disks variables (example 0,1#2,3#4#5#6)
+    names -> The names of the volume groups and logical volumes (example data#log#shared#usrsap#backup)
+    lv_sizes -> The size in % (from available space) dedicated for each logical volume and folder (example 50#50#100#100#100)
+    paths -> Folder where each volume group will be mounted (example /hana/data,/hana/log#/hana/shared#/usr/sap#/hana/backup#/sapmnt/)
+  EOF
+}
+
--- salt/hana_node/mount/init.sls
+++ salt/hana_node/mount/init.sls
@@ -2,7 +2,7 @@ include:
   - hana_node.mount.packages
   {% if grains['provider'] == 'azure' %}
   - hana_node.mount.azure
-  {%- if grains['provider'] in ['gcp', 'libvirt', 'openstack'] %}
+  {%- if grains['provider'] in ['aws', 'gcp', 'libvirt', 'openstack'] %}
   - hana_node.mount.lvm
   {% else %}
   - hana_node.mount.mount
